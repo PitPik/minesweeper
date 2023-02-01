12d515b856179eb83d2336add0a65d1a3015768e
Peter Dematté <PitPik@users.noreply.github.com>
2023-02-01 13:19:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@
       board[n] = [];
       cbRow && cbRow(board[n][m]);
       for (var m = rowcol[1]; m--; ) {
-        board[n][m] = { mark: '', done: false, mine: false, yx: [n, m] };
+        board[n][m] = { mark: '', done: false, mine: false, x: n, y : m };
         cbCol && cbCol(board[n][m]);
       }
       cbRowEnd && cbRowEnd(board[n][m]);
@@ -27,13 +27,9 @@
   }
 
   function createMines(board, rowcol, mineCount) {
-    var math = Math;
-    var random = math.random;
-    var floor = math.floor;
-
     while (mineCount) {
-      var row = floor(random() * rowcol[0]);
-      var col = floor(random() * rowcol[1]);
+      var row = Math.floor(Math.random() * rowcol[0]);
+      var col = Math.floor(Math.random() * rowcol[1]);
 
       if (!board[row][col].mine) {
         board[row][col].mine = true;
@@ -59,11 +55,11 @@
     return out;
   };
 
-  function checkAround(board, item, cb, checkAround, foundMarked) {
-    lookAround(board, item.yx[0], item.yx[1], function(row, col, foundItem) {
+  function checkAround(board, item, cb, doCheck, foundMarked) {
+    lookAround(board, item.x, item.y, function(row, col, foundItem) {
       if (foundMarked !== undefined) {
         if (foundItem.mark === marked) foundMarked++;
-      } else if (checkAround || !foundItem.mine && !foundItem.done) {
+      } else if (doCheck || !foundItem.mine && !foundItem.done) {
         checkItem(board, foundItem, cb, undefined);
       }
     });
@@ -71,20 +67,17 @@
   }
 
   function checkAll(items) {
-    for (var x = items.length; x--; ) {
-      if (!items[x].mine || items[x].mark !== marked) return;
-    }
+    for (var x = items.length; x--; ) if (!items[x].mine || items[x].mark !== marked) return;
     return true;
   }
 
   function checkItem(board, item, cb, doMark) {
     if (item.done && doMark !== undefined) {
-      delete checkItem._win && checkAround(board, item, cb, false, 0) ===
-        item.arround && checkAround(board, item, cb, true);
+      delete checkItem._win;
+      checkAround(board, item, cb, false, 0) === item.arround && checkAround(board, item, cb, true);
       if (checkItem._win === false) return false;
     } else if (doMark && !item.done) {
-      item.mark = item.mark === marked ? 'open' :
-        item.mark === 'open' ? '' : marked;
+      item.mark = item.mark === marked ? 'open' : item.mark === 'open' ? '' : marked;
     } else if (item.mine && item.mark !== marked) {
       getItems(board, 'mine', true, []).forEach(function(_item) {
         _item.mark = _item === item ? 'mine last' : 'mine';
@@ -126,17 +119,11 @@
     views.interval = clearInterval(views.interval);
     views.timer[textContent] = '0';
 
-    return {
-      won: undefined,
-      row: +values[0],
-      col: +values[1],
-      mines: +values[2],
-    };
+    return { won: undefined, row: +values[0],  col: +values[1], mines: +values[2] };
   }
 
   function createView() {
     var state = reset();
-    var gameBoard = views.gameBoard;
     var html = '';
     var board = createBoard([state.row, state.col], state.mines,
       function() { html += '<tr>'; },
@@ -144,7 +131,7 @@
       function() { html += '</tr>'; }
     );
 
-    gameBoard.innerHTML = html;
+    views.gameBoard.innerHTML = html;
     updatUIView(board, state);
 
     views.check = function(e) { // fresh event listener
@@ -155,13 +142,11 @@
       if (col === undefined) return updatUIView(board, state);
       if (state.won !== undefined) return;
 
-      if (!views.interval) { // start timer
-        views.interval = setInterval(function() {
-          views.timer[textContent] = +views.timer[textContent] + 1;
-        }, 1000);
-      }
+      if (!views.interval) views.interval = setInterval(function() {
+        views.timer[textContent]++;
+      }, 1000);
       state.won = checkItem(board, board[row][col], function(item) {
-        updateItemView(gameBoard, item);
+        updateItemView(views.gameBoard, item);
       }, e.type === 'contextmenu');
       updatUIView(board, state);
       if (state.won !== undefined) clearInterval(views.interval);
@@ -171,22 +156,19 @@
   function updatUIView(board, state) {
     body[className] = state.won ? 'win' : state.won === false ? 'loose' : '';
     if (state.won === false) return;
-    views.counter[textContent] = state.mines -
-      getItems(board, 'mark', 'marked', []).length;
+    views.counter[textContent] = state.mines - getItems(board, 'mark', 'marked', []).length;
   }
 
   function updateItemView(gameBoard, item) {
-    var cell = gameBoard.children[item.yx[0]].children[item.yx[1]];
+    var cell = gameBoard.children[item.x].children[item.y];
 
-    cell[className] = item.done ?
-      ' color-' + item.arround :
-      ' hidden ' + item.mark;
+    cell[className] = item.done ? ' color-' + item.arround : ' hidden ' + item.mark;
     cell[textContent] = item.done ? item.arround : '';
   }
 
   // ---------- init all
 
-  createView(gameBoard);
+  createView();
 
   gameBoard[aEL]('mousedown', function() { body[className] += ' huh' });
   gameBoard[aEL]('click', function(e) { views.check(e) });
